70a4ec0f165659111811578cc9717aa106ba13e4
leaisrevolution <leaisrevolution@gmail.com>
2022-12-13 13:40:21 +0900

    [Add] 12줄 코드 프로그래밍

--- TypeScriptStudy/fastcampus/part3/app.js
+++ TypeScriptStudy/fastcampus/part3/app.js
@@ -1,18 +1,16 @@
 const ajax = new XMLHttpRequest();
-const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 
-ajax.open('GET', NEWS_URL, false);
+ajax.open("GET", NEWS_URL, false);
 ajax.send();
 
 const newsFeed = JSON.parse(ajax.response);
-const ul = document.createElement('ul');
+const ul = document.createElement("ul");
 
-for(let i = 0; i < 10; i++) {
-  const li = document.createElement('li');
-
+for (let i = 0; i < 10; i++) {
+  const li = document.createElement("li");
   li.innerHTML = newsFeed[i].title;
-
   ul.appendChild(li);
 }
 
-document.getElementById('root').appendChild(ul);
+document.getElementById("root").appendChild(ul);
