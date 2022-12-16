f57ac41fe8c33a79c2bac8933df84297644a5d84
leaisrevolution <leaisrevolution@gmail.com>
2022-12-16 18:15:22 +0900

    [Add] 중복된 코드 제거 및 인터페이스 소개

--- TypeScriptStudy/quiz/1_todo/src/index.ts
+++ TypeScriptStudy/quiz/1_todo/src/index.ts
@@ -1,9 +1,20 @@
+// type Todo = {
+//   id: number;
+//   title: string;
+//   done: boolean;
+// }
+
+interface Todo {
+  id: number;
+  title: string;
+  done: boolean;
+}
 
 // let todoItems: object[];
-let todoItems: {id: number; title: string; done: boolean}[];
+let todoItems: Todo[];
 
 // api
-function fetchTodoItems(): {id: number; title: string; done: boolean}[] {
+function fetchTodoItems(): Todo[] {
   const todos = [
     { id: 1, title: '안녕', done: false },
     { id: 2, title: '타입', done: false },
@@ -20,7 +31,7 @@ function fetchTodos(): object[] {
 
 
 // 구체적인 타입 정의
-function addTodo(todo: {id: number; title: string; done: boolean}): void {
+function addTodo(todo: Todo): void {
   todoItems.push(todo);
 }
 
@@ -28,7 +39,7 @@ function deleteTodo(index: number): void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index: number, todo: {id: number; title: string; done: boolean}) : void{
+function completeTodo(index: number, todo: Todo) : void{
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
