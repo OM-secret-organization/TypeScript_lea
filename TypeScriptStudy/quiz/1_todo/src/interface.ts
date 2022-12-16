7963c2c7b201a7d9783ba93ea3831e585f5a2f36
leaisrevolution <leaisrevolution@gmail.com>
2022-12-16 18:31:14 +0900

    [Add] 함수의 인자를 정의하는 인터페이스

--- TypeScriptStudy/quiz/1_todo/src/interface.ts
+++ TypeScriptStudy/quiz/1_todo/src/interface.ts
@@ -3,7 +3,19 @@ interface User {
   name: string;
 }
 
+// 변수에 인터페이스 활용
 let lea: User = {
   age: 13,
   name: '레아'
 }
+
+function getUser(user: User) {
+  console.log(user);
+}
+
+
+const LEA = {
+  name: 'lea',
+  age: 100
+}
+getUser(LEA);
