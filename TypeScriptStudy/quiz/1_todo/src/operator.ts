0f931b89aef7ba242bd5397f187d3e227665480a
leaisrevolution <leaisrevolution@gmail.com>
2022-12-23 12:50:27 +0900

    [Add] settings.json

--- TypeScriptStudy/quiz/1_todo/src/operator.ts
+++ TypeScriptStudy/quiz/1_todo/src/operator.ts
@@ -9,20 +9,20 @@ function logMessage(value: string | number): void {
   console.log(value);
 }
 
-logMessage("hi");
+logMessage('hi');
 logMessage(100); //err
 
 let lea: string | number | boolean;
 
 function leaMessage(value: string | number) {
-  if (typeof value === "number") {
+  if (typeof value === 'number') {
     value.toLocaleString();
   }
-  if (typeof value === "string") {
+  if (typeof value === 'string') {
     value.toString();
   }
 
-  throw new TrypeError("value must be string or number");
+  throw new TrypeError('value must be string or number');
 }
 
 interface Developer {
@@ -40,3 +40,5 @@ function askSomeone(someone: Developer | Person): void {
   someone.age;
   someone.skill;
 }
+
+askSomeone({ name: 'hi', age: 30, skill: 'hi' });
