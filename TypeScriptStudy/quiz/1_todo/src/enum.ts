453ae69f139e1ebc0e339ff91243d8c798a31313
leaisrevolution <leaisrevolution@gmail.com>
2022-12-26 11:44:02 +0900

    [Add] 숫자형 이넘

---
+++ TypeScriptStudy/quiz/1_todo/src/enum.ts
@@ -0,0 +1,15 @@
+// Enums, 특정 갑들의 집합을 의미하는 자료형
+
+// 나이키
+// 아디다스
+// 뉴발란스
+
+//숫자형 이넘
+enum Shoes {
+  Nike,
+  Adidas,
+}
+
+// 별도의 값을 지정하지 않으면 숫자형 이넘으로 취급
+let myShoes = Shoes.Nike;
+console.log(myShoes); //0
