df78680e37baa00c9d9e04d8f77a657a06673e70
leaisrevolution <leaisrevolution@gmail.com>
2022-12-27 10:50:42 +0900

    [Add] 클래스 소개

---
+++ TypeScriptStudy/quiz/1_todo/src/class.js
@@ -0,0 +1,13 @@
+// ES2015 (ES6)
+
+class Person {
+  // 클래스 로직
+  constructor(name, age) {
+    console.log('생성 되었음');
+    this.name = name;
+    this.age = age;
+  }
+}
+
+let Lea = new Person('레아', 31);
+console.log(lea);
