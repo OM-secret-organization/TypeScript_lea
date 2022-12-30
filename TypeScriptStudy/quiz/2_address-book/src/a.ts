b88d34fdabbf80c2a5562c92b3be694a08110ed2
leaisrevolution <leaisrevolution@gmail.com>
2022-12-30 15:00:44 +0900

    [Add] API 함수 타입 정의

---
+++ TypeScriptStudy/quiz/2_address-book/src/a.ts
@@ -0,0 +1,16 @@
+function fetchItems(): string[] {
+  const items = ['a', 'b', 'c'];
+  return items;
+}
+
+let result = fetchItems();
+console.log(result);
+
+function fetchItems2(): Promise<string[]> {
+  let items: string[] = ['a', 'b', 'c'];
+  return new Promise(function (resolve) {
+    resolve(items);
+  });
+}
+
+fetchItems2();
