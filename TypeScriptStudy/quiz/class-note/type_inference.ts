c4c7fba4da8d1242a88ebbcdb5825b1cdafbf913
leaisrevolution <leaisrevolution@gmail.com>
2022-12-30 15:44:48 +0900

    [Add] 인터페이스와 제네릭을 이용한 타입 추론 방식

--- TypeScriptStudy/quiz/class-note/type_inference.ts
+++ TypeScriptStudy/quiz/class-note/type_inference.ts
@@ -1,3 +1,4 @@
+//타입 추론 기본
 let a = 10;
 let b = 'string';
 
@@ -6,3 +7,12 @@ function getA(a = 10) {
   let c = 'lea';
   return a + c; //10lea -> string
 }
+
+// 타입 추론 2
+interface Dropdown<T> {
+  value: T;
+  title: string;
+}
+
+let shoppingItem: Dropdown<string> = { value: 'lea', title: 'hi' };
+let shoppingItem: Dropdown<number> = { value: 3, title: 'hi' };
