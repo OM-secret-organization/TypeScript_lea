bfe3e77606ae3ff63ad0a617008fdaaa1771d702
leaisrevolution <leaisrevolution@gmail.com>
2023-03-15 17:19:39 +0900

    git push

---
+++ TypeScriptStudy/nextjs_front_back/server/src/server.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import morgan from 'morgan';
+import { AppDataSource } from './data-source';
+
+import authRoutes from './routes/auth';
+
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+const app = express();
+const origin = process.env.ORIGIN;
+app.use(
+  cors({
+    origin,
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(morgan('dev'));
+dotenv.config();
+
+app.get('/', (_, res) => res.send('running'));
+app.use('/api/auth', authRoutes);
+
+app.use(express.static('public'));
+
+let port = 4000;
+app.listen(port, async () => {
+  console.log(`server running at ${process.env.APP_URL}`);
+
+  AppDataSource.initialize()
+    .then(() => {
+      console.log('database initialized');
+    })
+    .catch((error) => console.log(error));
+});
