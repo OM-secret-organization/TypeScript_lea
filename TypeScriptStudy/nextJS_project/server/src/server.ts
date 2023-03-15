a5d963f58f34fe379644a21eafa68c34bb11d7d5
leaisrevolution <leaisrevolution@gmail.com>
2023-03-15 16:15:00 +0900

    [Edit] Server

--- TypeScriptStudy/nextJS_project/server/src/server.ts
+++ TypeScriptStudy/nextJS_project/server/src/server.ts
@@ -1,23 +1,32 @@
 import express from 'express';
 import morgan from 'morgan';
 import { AppDataSource } from './data-source';
+
 import authRoutes from './routes/auth';
+
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 const app = express();
-const origin = 'http://locallhost:3000';
-
-app.use(cors({ origin }));
-
+const origin = process.env.ORIGIN;
+app.use(
+  cors({
+    origin,
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(morgan('dev'));
+dotenv.config();
+
 app.get('/', (_, res) => res.send('running'));
 app.use('/api/auth', authRoutes);
 
-let port = 4000;
+app.use(express.static('public'));
 
+let port = 4000;
 app.listen(port, async () => {
-  console.log(`server running at http://localhost:${port}`);
+  console.log(`server running at ${process.env.APP_URL}`);
 
   AppDataSource.initialize()
     .then(() => {
