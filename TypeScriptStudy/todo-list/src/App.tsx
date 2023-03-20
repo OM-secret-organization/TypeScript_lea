9ef8743b08a0d9405ecbcb5349cf46b9946fc002
leaisrevolution <leaisrevolution@gmail.com>
2023-03-20 14:56:07 +0900

    [Add] Recoil Env

--- TypeScriptStudy/todo-list/src/App.tsx
+++ TypeScriptStudy/todo-list/src/App.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { atom, selector, useRecoilValue } from 'recoil';
+import axios from 'axios';
 
+const todoIdState = atom({
+  key: 'idState/uuid()',
+  default: 1,
+});
+
+const todoItemQuery = selector({
+  key: 'itemQuery',
+  get: async ({ get }) => {
+    const id = get(todoIdState);
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/todos/${id}`
+    );
+    return response.data;
+  },
+});
 
 function App() {
-  return (
-
-  );
+  const data = useRecoilValue(todoItemQuery);
+  console.log('data', data);
+  return <div>{data.title}</div>;
 }
 
 export default App;
